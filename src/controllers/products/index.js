5b304f14a83ff0e888b721e953102cd4634c2dd5
Tomi Adebanjo <tomiadebanjo@gmail.com>
2019-04-17 17:48:39 +0100

    feat(endpoint): implement get products by department functionality
    - create method to get products by department
    - write tests for the get products by category endpoint
    
    [Delivers #165356076]

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -3,7 +3,22 @@ import ProductController from './ProductController';
 
 const productRouter = express.Router();
 
+// Get all products
 productRouter.get('/products', ProductController.getAllProducts);
-productRouter.get('/products/inCategory/:category_id', ProductController.getProductsByCategory);
+
+// Get products by category
+productRouter.get(
+  '/products/inCategory/:category_id',
+  ProductController.getProductsByCategory
+);
+
+// Get products by department
+productRouter.get(
+  '/products/inDepartment/:department_id',
+  ProductController.getProductsByDepartment
+);
+
+// Get products by search string
+// productRouter.get('/products/search', ProductController.getProductsBySearchString);
 
 export default productRouter;
