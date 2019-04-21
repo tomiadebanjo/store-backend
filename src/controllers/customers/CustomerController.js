961f8f0cfee2d2af5ed1c99f8d9d2d2854be122a
Tomi Adebanjo <tomiadebanjo@gmail.com>
2019-04-21 23:56:31 +0100

    add user input validations

--- src/controllers/customers/CustomerController.js
+++ src/controllers/customers/CustomerController.js
@@ -1,11 +1,24 @@
+import { validationResult } from 'express-validator/check';
 import CustomerService from '../../services/CustomerService';
 import JwtHelper from '../../helpers/JwtHelper';
 
 class CustomerController {
   static async signUp(req, res) {
+    const result = validationResult(req);
+
+    console.log(result.mapped(), 'result');
+    console.log(req.body);
+
+    if (result.array()) {
+      return res.send({
+        // errors: result(req).array()
+        errors: result.array()
+      });
+    }
+
     const { body } = req;
     const [user, created] = await CustomerService.findOrCreateUser(body);
-    // console.log(user);
+
     if (!created) {
       return res.status(409).send({
         error: {
@@ -13,12 +26,15 @@ class CustomerController {
         }
       });
     }
-    console.log(user.get({
-      plain: true
-    }));
+
+    const customer = user.toJSONData();
+    const expiresIn = '24h';
+    const accessToken = JwtHelper.generateToken({ data: customer, expiresIn });
+
     return res.status(201).send({
-      user,
-      created
+      customer: user.toJSONData(),
+      accessToken,
+      expires_in: expiresIn
     });
   }
 
@@ -35,7 +51,8 @@ class CustomerController {
         }
       });
     }
-    const customer = { ...user.toJSONData() };
+
+    const customer = user.toJSONData();
     const expiresIn = '24h';
     const accessToken = JwtHelper.generateToken({ data: customer, expiresIn });
     return res.status(200).send({
