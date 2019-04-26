87e769b6b1db01c736a1d552f8f472c6a66c38dd
Tomi Adebanjo <tomiadebanjo@gmail.com>
2019-04-26 16:07:17 +0100

    add error loggers

---
+++ src/helpers/logger.js
@@ -0,0 +1,31 @@
+import winston from 'winston';
+
+const { createLogger, format, transports } = winston;
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  defaultMeta: { service: 'Turin Backend' },
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.simple()
+    )
+  }));
+}
+
+export default logger;
