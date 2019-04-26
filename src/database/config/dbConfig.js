d8957022056b7371c4e5ec15acfb4f11c8d4a375
Tomi Adebanjo <tomiadebanjo@gmail.com>
2019-04-26 15:08:39 +0100

    setup circle ci

--- src/database/config/dbConfig.js
+++ src/database/config/dbConfig.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: 'root',
-    password: 'root',
-    database: 'turin',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
   test: {
-    username: 'root',
-    password: 'root',
-    database: 'turin_test',
-    host: '127.0.0.1',
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: 'root',
-    database: 'turin',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
 };
